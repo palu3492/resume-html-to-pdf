60b8d469134f4dbbabee133b3ac13f578bf4299a
Alex Palumbo <palu3492@stthomas.edu>
2020-01-26 15:44:33 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/resume.html'));
+});
+
+app.listen(8080);
